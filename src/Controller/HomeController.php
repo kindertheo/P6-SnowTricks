f56c7b8ac1e4d936a8229d277a22c8dadd8a6b92
Théo <kinder.theo@gmail.com>
2020-04-21 15:36:17 +0200

    Create HomeController

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\TricksRepository;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(TricksRepository $repo)
+    {
+        $tricks = $repo->findAll();
+        return $this->render('home/index.html.twig', [
+            'tricks' => $tricks
+        ]);
+    }
+}
