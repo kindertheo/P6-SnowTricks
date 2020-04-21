6ca7aeb08ac677e0ee486b3b55d77a3ac15519a5
Théo <kinder.theo@gmail.com>
2020-04-21 15:40:20 +0200

    create trickstype

---
+++ src/Form/TricksType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Tricks;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TricksType extends ApplicationType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, $this->getConfiguration("Nom du tricks", "Entrez le nom"))
+            ->add('description', TextareaType::class, $this->getConfiguration("Description", "Entrez une description représentant le trick"))
+            ->add('image', UrlType::class, $this->getConfiguration("Image principale", "Lien de l'image"))
+            ->add('video', UrlType::class, $this->getConfiguration("Vidéo", "Lien de la vidéo", ["required" => false]))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Tricks::class,
+        ]);
+    }
+}
