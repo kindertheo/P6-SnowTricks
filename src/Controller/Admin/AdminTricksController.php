42e4362bfcad5b3b6ba24823fdbabbab42d7c329
Théo <kinder.theo@gmail.com>
2020-04-25 20:34:42 +0200

    manage tricks as admin

---
+++ src/Controller/Admin/AdminTricksController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Tricks;
+use App\Repository\TricksRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminTricksController extends AbstractController
+{
+    /**
+     * @Route("/admin/tricks", name="admin_tricks")
+     * @param TricksRepository $tricks
+     * @return Response
+     */
+    public function index(TricksRepository $tricks)
+    {
+        return $this->render('admin/admin_tricks/index.html.twig', [
+            'tricks' => $tricks->findAll()
+        ]);
+    }
+
+    /**
+     * @Route("admin/tricks/delete/{id}", name="admin_tricks_delete")
+     * @param Tricks $tricks
+     * @param EntityManagerInterface $manager
+     *
+     * @return RedirectResponse
+     */
+    public function delete(Tricks $tricks, EntityManagerInterface $manager){
+        $name = $tricks->getId();
+        $manager->remove($tricks);
+        $manager->flush();
+
+        $this->addFlash("success", "Le tricks '$name' a bien été supprimé");
+
+        return $this->redirectToRoute("admin_comment");
+    }
+}
