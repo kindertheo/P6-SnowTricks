283df7c291962d433571f7cf393cbae045fb9069
Théo <kinder.theo@gmail.com>
2020-04-26 19:36:04 +0200

    image entity

---
+++ src/Entity/Image.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ImageRepository")
+ */
+class Image
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Tricks", inversedBy="images")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $Tricks;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getTricks(): ?Tricks
+    {
+        return $this->Tricks;
+    }
+
+    public function setTricks(?Tricks $Tricks): self
+    {
+        $this->Tricks = $Tricks;
+
+        return $this;
+    }
+}
