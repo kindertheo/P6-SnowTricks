9b5ad2fd899417ff3515428210b73b107c974a2e
Théo <kinder.theo@gmail.com>
2020-04-28 01:49:15 +0200

    add collectiontype

--- src/Form/TricksType.php
+++ src/Form/TricksType.php
@@ -3,7 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Tricks;
-use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -14,11 +14,17 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 class TricksType extends ApplicationType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+    {/*TODO Rectifier le placeholder du image upload*/
         $builder
             ->add('name', TextType::class, $this->getConfiguration("Nom du tricks", "Entrez le nom"))
             ->add('description', TextareaType::class, $this->getConfiguration("Description", "Entrez une description représentant le trick"))
-            ->add('image', FileType::class, $this->getConfiguration("Image principale", "Lien de l'image"))
+            ->add('main_image', FileType::class, $this->getConfiguration("Image principale", "Image principale"))
+            ->add('images', CollectionType::class, array(
+                "entry_type"    => ImageType::class,
+                "allow_add"     => true,
+                "allow_delete"  => true,
+                'entry_options' => ['label' => false],
+            ))
             ->add('video', UrlType::class, $this->getConfiguration("Vidéo", "Lien de la vidéo", ["required" => false]))
         ;
     }
