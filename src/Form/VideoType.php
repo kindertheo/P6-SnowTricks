db711eb32e9dfc5e0ff0277ff43e07ca1f745af4
Théo <kinder.theo@gmail.com>
2020-04-29 12:39:22 +0200

    form video

---
+++ src/Form/VideoType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Video;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class VideoType extends ApplicationType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, $this->getConfiguration("Titre de la vidéo", "Titre"))
+            ->add('video', UrlType::class, $this->getConfiguration("Lien de la vidéo", "Youtube, dailymotion.."))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Video::class,
+        ]);
+    }
+}
