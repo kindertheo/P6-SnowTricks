23cc0e3ab1481d4dbde2932a17c7686d3d40ba65
Théo <kinder.theo@gmail.com>
2020-05-02 02:39:52 +0200

    creation videoservice

---
+++ src/Service/VideoService.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace App\Service;
+
+
+use App\Entity\Tricks;
+use App\Entity\Video;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\RouterInterface;
+
+class VideoService extends AbstractController
+{
+
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function addVideo(Video $video, Tricks $tricks)
+    {
+        $video->setTricks($tricks);
+
+        $this->entityManager->persist($video);
+        $this->entityManager->flush();
+    }
+
+    public function deleteVideo(Video $video)
+    {
+        $id = $video->getTricks()->getId();
+        $this->entityManager->remove($video);
+        $this->entityManager->flush();
+
+        return $this->redirectToRoute("tricks_update", ['id' => $id]);
+    }
+}
