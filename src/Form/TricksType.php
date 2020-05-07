5f7ecc5161fb83acfbeb2b20abe0e071c4b03ae5
Théo <kinder.theo@gmail.com>
2020-05-07 05:44:16 +0200

    add category

--- src/Form/TricksType.php
+++ src/Form/TricksType.php
@@ -18,6 +18,7 @@ class TricksType extends ApplicationType
         $builder
             ->add('name', TextType::class, $this->getConfiguration("Nom du tricks", "Entrez le nom"))
             ->add('description', TextareaType::class, $this->getConfiguration("Description", "Entrez une description représentant le trick"))
+            ->add('category', TextType::class, $this->getConfiguration("Catégorie", "Entrez une catégorie"))
             ->add("mainImage", FileType::class);
         ;
     }
--- src/Form/UpdateProfileType.php
+++ src/Form/UpdateProfileType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -20,7 +21,8 @@ class UpdateProfileType extends ApplicationType
             ->add('firstName', TextType::class, $this->getConfiguration("Prénom", "Prénom"))
             ->add('lastName', TextType::class, $this->getConfiguration("Nom de famille", "Nom de famille"))
             ->add('email', EmailType::class, $this->getConfiguration("Adresse email", "Adresse email"))
-            ->add('picture', UrlType::class, $this->getConfiguration("Lien vers votre image de profil", "Photo de profil"))
+            ->add('picture', FileType::class, $this->getConfiguration("Lien vers votre image de profil", "Photo de profil",
+                ['data_class' => null, 'required' => false,  'empty_data' => false]))
             ->add('introduction', TextareaType::class, $this->getConfiguration("Introduction",
                 "Une petite introduction qui sera affichée sur votre page de profil", ['required' => false]))
         ;
